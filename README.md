992d0bb867f46f024c62b4d021403bd80f591e50
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2020-07-11 15:01:51 +0200

    Primo dump dei dati di giugno

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# medici-asl-torino
-I medici di medicina generale della ASL di Torino
+# Medici ASL Torino
+
+Questo repository contiene i dati dei medici di medicina generale della ASL di Torino in formato JSON.
+I JSON sono creati a partire dai dati presi dal sito della ASL di Torino.
+
+Il codice che converte i dati in JSON si trova nel repository [parse-medici-torino](https://github.com/xrmx/parse-medici-torino) assieme a tutti i dettagli tecnici per replicare il processo.
---
+++ convert.sh
@@ -0,0 +1,17 @@
+PARSE=../parse-medici-torino/parse.py
+
+PDFS="Medici-di-Medicina-Generale-Circoscrizione-1.pdf
+Medici-di-Medicina-Generale-Circoscrizione-2.pdf
+Medici-di-Medicina-Generale-Circoscrizione-3.pdf
+Medici-di-Medicina-Generale-Circoscrizione-4-1.pdf
+Medici-di-Medicina-Generale-Circoscrizione-5.pdf
+Medici-di-Medicina-Generale-Circoscrizione-6.pdf
+Medici-di-Medicina-Generale-Circoscrizione-7.pdf
+Medici-di-Medicina-Generale-Circoscrizione-8.pdf"
+
+for pdf in $PDFS; do
+	curl http://www.aslcittaditorino.it/wp-content/uploads/2020/06/$pdf > $pdf
+	filename=$(basename -s.pdf $pdf)
+	pdftotext $pdf $filename.txt
+	cat $filename.txt | python3 $PARSE > $filename.json 2> $filename.log
+done
