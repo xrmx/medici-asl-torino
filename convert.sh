035ad33c19895308a31d39f77ae35365bc82f3ff
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2020-07-19 16:04:50 +0200

    Dump del geojson da visualizzare su mappa

--- convert.sh
+++ convert.sh
@@ -1,4 +1,5 @@
 PARSE=../parse-medici-torino/parse.py
+TOGEOJSON=../parse-medici-torino/json2geojson.py
 
 PDFS="Medici-di-Medicina-Generale-Circoscrizione-1.pdf
 Medici-di-Medicina-Generale-Circoscrizione-2.pdf
@@ -9,9 +10,14 @@ Medici-di-Medicina-Generale-Circoscrizione-6.pdf
 Medici-di-Medicina-Generale-Circoscrizione-7.pdf
 Medici-di-Medicina-Generale-Circoscrizione-8.pdf"
 
+JSONS=""
+
 for pdf in $PDFS; do
 	curl http://www.aslcittaditorino.it/wp-content/uploads/2020/06/$pdf > $pdf
 	filename=$(basename -s.pdf $pdf)
 	pdftotext $pdf $filename.txt
 	cat $filename.txt | python3 $PARSE > $filename.json 2> $filename.log
+	JSONS="$JSONS $filename.json"
 done
+
+python3 $TOGEOJSON $JSONS > medici.geojson
